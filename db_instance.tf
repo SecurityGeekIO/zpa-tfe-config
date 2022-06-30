f4a28ed3614078230a1638716e017735532bc9bf
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-06-29 20:54:00 -0700

    IaC CNAPP Scan test

--- db_instance.tf
+++ db_instance.tf
@@ -9,16 +9,15 @@ resource "aws_kms_key" "km_db_kms_key" {
 }
 
 resource "aws_db_instance" "km_db" {
-#zs:skip=ZS-AWS-00032 Ignore for Testing
-#zs:skip=ZS-AWS-00013 Ignore for Testing
+
     name = "km_db_${var.environment}"
     allocated_storage = 20
     engine = "postgres"
     engine_version = "10.6"
     instance_class = "db.t3.medium"
     storage_type = "gp2"
-    backup_retention_period = "35"
-    storage_encrypted = true
+    # backup_retention_period = "35"
+    # storage_encrypted = true
 }
 
 

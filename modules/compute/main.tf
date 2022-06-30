a262f628929bd3b298d17beec2c075f2ecfd8e30
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-06-29 22:22:27 -0700

    Added modules

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -9,7 +9,6 @@ resource "aws_kms_key" "km_db_kms_key" {
 }
 
 resource "aws_db_instance" "km_db" {
-
     name = "km_db_${var.environment}"
     allocated_storage = 20
     engine = "postgres"
--- s3.tf
+++ s3.tf
@@ -1,12 +1,14 @@
 module "s3_bucket" {
-  #zs:skip=ZS-AWS-00035 This is a test
-  source = "terraform-aws-modules/s3-bucket/aws"
+
+  source = "./modules/storage"
 
   bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
-    enabled = true
+    enabled = false
   }
 
 }
+
+  #zs:skip=ZS-AWS-00035 This is a test
